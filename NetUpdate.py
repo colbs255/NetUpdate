1f1f6fe973aac57ab89f2865576f4d9d50f55314
colbychance <duncanchance11@gmail.com>
2018-12-08 20:31:43 -0500

    can now mostly end by closing the socket

--- NetUpdate.py
+++ NetUpdate.py
@@ -25,31 +25,49 @@ class NetUpdate:
         if not self._listen_thread:
             return
         self._is_active = False
-        while self._listen_thread.isAlive:
+        while self._listen_thread.isAlive():
             continue
         self._socket.close()
         self._listen_thread = None
+        self._socket = None
     
     def _init_socket(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.bind((self._host, self._port_number))
+        self._socket.setblocking(0)
     
     def _listener(self):
         self._socket.listen(self._max_connections)
-        while self._is_active:
-            connection, addr = self._socket.accept()
+        while True:
+            connection = None
+            addr = None
+            while self._is_active and not connection:
+                try:
+                    c, a = self._socket.accept()
+                    connection = c
+                    addr = a   
+                except BlockingIOError:
+                    pass        
+            if not self._is_active:
+                return
             print('New client')
             a_thread = Thread(target=self._handle_client, args=(connection, addr), daemon=True)
             a_thread.start()
             
     def _handle_client(self, connection, addr):
-        while self._is_active:
-            data = connection.recv(2000)
+        while True:
+            data = None
+            while self._is_active and not data:
+                try:
+                    data = connection.recv(2000)
+                except BlockingIOError:
+                    pass
+            if not self._is_active:
+                return
             print('req reveived')
             val = str(self.callback())
             msg = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: {}\n\n{}".format(len(val), val)
             connection.send(msg.encode())
 
-#TODO: when I end, it needs to stop listening
-    #as of now it is stuck in the recv part
-#TODO: same with the handle client part
+#TODO: need a timeout functionality so i dont have a ton of threads
+#TODO: need to make sure the portnumber is not being used again before i restart
